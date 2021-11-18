124c1c5f0d9427a7e6bd49e8dbf50d37a0d30886
TheCoderGuru <shashreeshachindrasamuel14@gmail.com>
2021-11-18 11:29:22 -0400

    Added another template

---
+++ README.md
@@ -0,0 +1,72 @@
+
+<br>
+
+<a href="https://www.npmjs.com/package/generator-bootstrap-boilerplate-template"><img src="https://nodei.co/npm/generator-bootstrap-boilerplate-template.png?downloadRank=true&downloads=true&downloadRank=true&stars=true" /></a>
+
+<br>
+
+A Yeoman generator that creates the file structure for your bootstrap project.
+
+
+<br>
+
+The file structure for the generator is as follows:
+
+```
+project-name/
+├── node_modules/
+│   └── bootstrap/
+│   └── popper.js/
+├── scss/
+│   └── custom.scss
+├── src/
+│   └── index.html
+│   └── index.js
+└── package.json
+```
+
+<br>
+
+## Instructions
+
+Install Yeoman globally: 
+
+```bash
+
+npm install -g yo 
+
+sudo npm install -g yo
+```
+
+<br>
+
+Then install the Bootstrap boilerplate generator globally: 
+
+```bash
+
+npm install -g generator-bootstrap-boilerplate-template
+
+sudo npm install -g generator-bootstrap-boilerplate-template
+```
+
+<br>
+
+Navigate to a directory you want to generate the files in, then run ```yo bootstrap-boilerplate-template```
+
+The output should look like the following:
+
+<img src="https://github.com/TheCoderGuru/generator-bootstrap-boilerplate-template/blob/main/Screenshot%202021-11-16%208.05.38%20PM.png" height="50%">
+
+<br>
+
+Select either ```template_1``` or ```template_2``` by using the <kbd>↓</kbd> and then await for the output.
+
+<br>
+
+<img src="https://github.com/TheCoderGuru/generator-bootstrap-boilerplate-template/blob/main/Screenshot%202021-11-16%208.21.34%20PM.png" height="50%">
+
+Thats it. You are now ready to start coding in Bootstrap :computer: 
+
+<br>
+
+####t **More website templates coming soon, stay tuned**
---
+++ generators/app/index.js
@@ -0,0 +1,139 @@
+'use strict';
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+const mkdirp = require('mkdirp');
+
+module.exports = class extends Generator {
+  prompting() {
+    // Have Yeoman greet the user.
+    this.log(
+      yosay(
+        `Welcome to the great ${chalk.red('generator-bootstrap-boilerplate-template')} generator!`
+      )
+    );
+
+    const prompts = [
+    {
+        type: 'input', 
+        name: 'name',
+        message: 'What would you like to name your website project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description of your website project'
+    },
+    {
+        type: 'input',
+        name: 'fullname',
+        message: 'Please enter your name that you would like to be used for this project'
+    },
+    {
+        type: 'input',
+        name: 'repo',
+        message: 'Please enter the url of your repository'
+    },
+    {
+        type: 'list',
+        name: 'option',
+        choices: [ 'template_1', 'template_2' ],
+        message: 'Which website template would you like to generate?'
+    }];
+
+    return this.prompt(prompts).then(props => {
+        this.props = props;
+    })
+    }
+
+
+    writing() {
+        if( this.props.option === "template_1" ){
+            this.fs.copy( 
+                this.templatePath( 'src/index.html' ), 
+                this.destinationPath( 'src/index.html' ) 
+            );
+            this.fs.copy( 
+                this.templatePath( 'scss/custom.scss' ), 
+                this.destinationPath( 'scss/custom.scss' ) 
+            );
+        } else {
+            mkdirp('bootstrap_template_2/src')
+            mkdirp( 'bootstrap_template_2/scss' )
+
+            this.fs.copy(
+                this.templatePath( 'index.html' ),
+                this.destinationPath( 'bootstrap_template_2/src/index.html' ) 
+            )
+
+            this.fs.copy(
+                this.templatePath( 'index.js' ),
+                this.destinationPath( 'bootstrap_template_2/src/index.js' ) 
+            )
+
+            this.fs.copy(
+                this.templatePath( 'custom.scss' ),
+                this.destinationPath( 'bootstrap_template_2/scss/custom.scss' ) 
+            )
+
+            this.fs.copy(
+                this.templatePath( 'images/' ),
+                this.destinationPath( 'bootstrap_template_2/images/' ) 
+            )
+        }
+
+            const pkg = require( '../../package.json')
+            pkg.name = this.props.name;
+            pkg.repo = this.props.repo;
+            pkg.version = '0.1.0';
+            pkg.description = this.props.description;
+            pkg.contributors = [ `${ this.props.fullname }`]
+            pkg.dependencies = undefined;
+            pkg.devDependencies = undefined;
+            pkg.files = undefined;
+            pkg.keywords = undefined;
+            pkg.publishConfig = undefined;
+            if( this.props.repo != null && this.props.repo.trim() != '' ){
+                pkg.repository = {
+                    "type": "git",
+                    "url": `git+${this.props.repo}.git`
+                };
+                pkg.bugs = {
+                    "url": `git+${this.props.repo}/issues`
+                };
+                pkg.homepage = `${this.props.repo}#readme`;
+            }
+            else {
+                pkg.repository = undefined;
+                pkg.bugs = undefined;
+                pkg.homepage = undefined;
+            }
+            pkg['scripts'] = {
+                dev: "parcel ./src/index.html",
+                prebuild: "npx rimraf build",
+                build: "parcel build --public-url ./ ./src/index.html --dist-dir build"
+            };
+            this.fs.writeJSON(this.destinationPath('package.json'), pkg)
+            this.log('Finished writing the package.json file')
+      
+    }
+
+
+	end() {
+		    
+        this.log( chalk.bold.green( 'Generated all the files.. Happy Bootstrapping' ) )
+            
+        this.log( ' ' )
+            
+        this.log( 'Thank you for using this generator, changes will be made to better improve your experience so do leave your valuable feedback.' )
+            
+        this.log( ' ' )
+
+        this.log('Type `npm run dev` to run the development server.');
+            
+        this.log( ' ' )
+            
+        this.log( 'Bootstrap Boilerplate Template | TheCoderGuru' )
+        
+    }
+};
